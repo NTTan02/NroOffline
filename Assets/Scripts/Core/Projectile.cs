8a3b58c6d6f507a2d492e8ee6826d0c009904260
ntt.tan0203 <dieternguyen8386@gmail.com>
2026-05-18 10:44:36 +0700

    Update Projectile.cs

--- Assets/Scripts/Core/Projectile.cs
+++ Assets/Scripts/Core/Projectile.cs
@@ -14,7 +14,6 @@ public class Projectile : MonoBehaviour
         atk       = playerATK;
         speed     = skillData.projectileSpeed;
 
-        // Xoay projectile theo hướng bay
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
